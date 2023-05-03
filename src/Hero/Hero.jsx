fad30cf32ef473def1b1b961ce0d33171a5b940c
EmanuelGalli <emanuel.galli@bootcamp-upgrade.com>
2023-05-03 21:08:22 +0200

    se agrega linkedin

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -19,11 +19,17 @@ const Hero = ({hero}) => {
           emanuelandresgalli@gmail.com
           </a>
         </p>
+        <div className="linkedin">
+        <img className="iconoss" src="./imagenes/icons8-linkedin-96.png" alt=""/> <div class="badge-base LI-profile-badge" data-locale="es_ES" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="emanuel-andrés-galli" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://es.linkedin.com/in/emanuel-andr%C3%A9s-galli?trk=profile-badge">Emanuel Andrés Galli</a></div>
+        </div> 
+              
         <p> <img className="iconoss" src="/imagenes/icons8-teléfono-celular-50.png" alt=""/> {hero.phone}</p>
         <p> <img className="iconos" src="./imagenes/icons8-github-48.png" alt=""/> <a href={hero.gitHub}>
             GitHub
           </a></p>
       </div>
+      
+              
         </div>
     )
 }
